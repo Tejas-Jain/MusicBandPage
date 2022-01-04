3c608996a903e22e17061880188217dbe15001db
Tejas Jain <1tejasjain@gmail.com>
2022-01-04 16:05:49 +0530

    Added Functionality to HomePage using js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var ItemAddBtns = document.getElementsByClassName('buy')
+console.log('working')
 for(let i=0; i<ItemAddBtns.length;i++)
 {
     ItemAddBtns[i].addEventListener('click', addCartItems)
@@ -6,19 +7,24 @@ for(let i=0; i<ItemAddBtns.length;i++)
 function addCartItems(event){
     var item=event.target.parentElement.parentElement;
     var title= item.getElementsByClassName('item-title')[0].innerHTML
-    var image = item.getElementsByTagName('img')[0].src
-    var price= item.querySelectorAll('.item-details span')[0].innerHTML
-    addItemToCart(title,image,price)
+    var image = item.getElementsByTagName('img')[0]
+        if(image!=undefined) image=image.src
+    var price= item.querySelectorAll('.item-details span')[0]
+        if(price!=undefined) price=price.innerHTML
+    var place= item.getElementsByClassName('item-place')[0]
+        if(place!=undefined) place=place.innerHTML
+    var date= item.getElementsByClassName('item-date')[0]
+        if(date!=undefined) date=date.innerHTML
+    addItemToCart(title,image,price,date,place)
 }
-
 function updateQty(event){
     var input=event.target
     if(input.value<=0)
         input.value=1;
     updateCartTotal();
 }
-function addItemToCart(title,image,price){
-    var existingRowsTitles=document.querySelectorAll(".cart .cartItemTitle");
+function addItemToCart(title,image,price,date,place){
+    var existingRowsTitles=document.querySelectorAll(".cartItemTitle");
     for(let i=0;i<existingRowsTitles.length;i++)
     {
         if(existingRowsTitles[i].innerHTML == title){
@@ -27,13 +33,26 @@ function addItemToCart(title,image,price){
         }
     }
     var cartRow= document.createElement('tr')
-    var cartRowContent= `
-        <td><img  src="${image}"  alt="" width=60rem><span class='cartItemTitle'>${title}</span></td>
-        <td>${price}</td>
-        <td>
-            <input class='quantity' type="number" value="1">
-            <button class="button btn not-buy" role="button">REMOVE</button>
-        </td>`
+    if(place!=undefined){
+        var cartRowContent= `
+            <td>${date}</td>
+            <td>${place}</td>
+            <td><span class='cartItemTitle'>${title}</span></td>
+            <td>
+                <input class='quantity' type="number" value="1">
+                <button class="button btn not-buy" role="button">REMOVE</button>
+            </td>`
+    }
+    if(image!=undefined){
+        var cartRowContent= `
+            <td><img  src="${image}"  alt="" width=60rem><span class='cartItemTitle'>${title}</span></td>
+            <td>${price}</td>
+            <td>
+                <input class='quantity' type="number" value="1">
+                <button class="button btn not-buy" role="button">REMOVE</button>
+            </td>`
+    }
+    var cartRow= document.createElement('tr')
     cartRow.innerHTML=cartRowContent
     document.querySelectorAll('.cart table')[0].appendChild(cartRow)
     cartRow.getElementsByClassName('not-buy')[0].addEventListener("click", removeCartItems)
